3a0a7d40b0c3ecdb9e3d621b9b582d874b2f3dd3
Mike Calvanese <mfcalvanese@gmail.com>
2014-10-14 22:36:40 -0400

    Added modules.js file to defined all app modules
    This is so that app js files can be loaded in any order, which simplifies the karma config

--- app/templates/app/app.js
+++ app/templates/app/app.js
@@ -1,5 +1,3 @@
-var <%= appname %> = angular.module('<%= appname %>', ['ngAnimate']);
-
 <%= appname %>.config(function() { });
 
 <%= appname %>.run(function() { });
---
+++ app/templates/app/modules.js
@@ -0,0 +1,1 @@
+var <%= appname %> = angular.module('<%= appname %>', ['ngAnimate']);
