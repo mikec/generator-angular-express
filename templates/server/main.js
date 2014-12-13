ca15208aefebe80a3d057b8ef6676237c42ee6e0
Mike Calvanese <mfcalvanese@gmail.com>
2014-12-13 11:30:04 -0500

    Removed extra line breaks

--- templates/server/main.js
+++ templates/server/main.js
@@ -2,12 +2,10 @@ var path = require('path');
 var url = require('url');
 var express = require('express');
 var cons = require('consolidate');
-var cookieParser = require('cookie-parser');
-<% if(filters.knexAndBookshelf) { %>
+var cookieParser = require('cookie-parser');<% if(filters.knexAndBookshelf) { %>
 var knexConfig = require('../knexfile');
 var knex = require('knex')(knexConfig.development);
-var bookshelf = require('bookshelf')(knex);
-<% } %>
+var bookshelf = require('bookshelf')(knex);<% } %>
 
 var app = module.exports = express();
 
@@ -20,16 +18,13 @@ var port = Number(process.env.PORT || 9999);
 
 app.set('views', path.join(rootPath, 'server'));
 app.engine('html', cons.handlebars);
-app.set('view engine', 'html');
-
-<% if(filters.knexAndBookshelf) { %>
+app.set('view engine', 'html');<% if(filters.knexAndBookshelf) { %>
 app.set('bookshelf', bookshelf);
 var models = require('require-directory')(module, './models');
 for(var modelName in models) {
     global[modelName] = models[modelName];
     app.set(modelName, models[modelName]);
-}
-<% } %>
+}<% } %>
 
 // if local dev
 app.use(require('connect-livereload')());
@@ -61,7 +56,6 @@ app.use('/', express.static(path.join(rootPath, 'app')));
 app.get('/', function(req, res) {
     renderIndex(req.config, res);
 });
-
 <% if(filters.knexAndBookshelf) { %>
 app.get('/api/users', function(req, res) {
     new User().fetchAll().then(function(users) {
@@ -72,7 +66,6 @@ app.get('/api/users', function(req, res) {
     });
 });
 <% } %>
-
 app.use(function(req, res) {
     res.redirect('/');
 });
